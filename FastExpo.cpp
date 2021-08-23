52f152edb813629a791cef7136e936290383b5b8
Lucas Gabriel <lucasgabrielgarriel@hotmail.com>
2021-08-23 16:58:19 -0300

    update

---
+++ FastExpo.cpp
@@ -0,0 +1,20 @@
+ll FastExpo(ll x, ll n, ll m) 
+{
+    x %= m;
+    ll res = 1;
+    while (n > 0) 
+    {
+        if (n & 1)
+            res = res * x % m;
+        x = x * x % m;
+        n >>= 1;
+    }
+    return res;
+}
+
+int main()
+{
+    ll B, P, M; //Base, Expoente, Mod
+    while (scanf("%lld %lld %lld", &B, &P, &M) != EOF)
+        printf("%lld\n", FastExpo(B, P, M));
+}
