7eb06bd1032979053595c1ebb29b9f7437544b9f
Lucas Gabriel <lucasgabrielgarriel@hotmail.com>
2021-10-25 23:19:14 -0300

    Update

--- template.cpp
+++ template.cpp
@@ -1,32 +1,25 @@
-#include <assert.h>
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
-#include <iomanip>
-#include <iostream>
-#include <limits.h>
-#include <list>
-#include <map>
-#include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <string.h>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 #define pi 3.141592653589793
 #define eulerconstant 0.577215664901532
-#define sz 550
+#define MAX_N 100005
+#define LG_N 17
 #define f first
 #define s second
-#define inf ~(1<<31) - 1
+#define inf ~(1<<31)
 #define inf64 1000000000000000000
 #define goldenratio ((double)(1 + sqrt(5)) / 2)
 #define isvalid(a_, b_, n_, m_) (a_ >= 0 && a_ < n_ && b_ >= 0 && b_ < m_)
+#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
+#define forr(i, l, r) for (int i = (int)(l); i < (int)(r); ++i)
+#define Mid ((l + r) >> 1)
+#define filIn freopen("input.txt", "r", (__acrt_iob_func(0)));
+#define filOut freopen("output.txt", "w", (__acrt_iob_func(1)));
 
 typedef pair<int, int> ii;
+typedef pair<int, pair<int, int>> iii;
 typedef pair<char, int> ci;
 typedef pair<string, int> si;
 typedef pair<int, string> is;
@@ -37,6 +30,7 @@ typedef vector<pair<int, int>> vii;
 typedef vector<pair< int, pair<int, int>>> viii;
 typedef vector<vector<int>> vvi;
 typedef long long ll;
+typedef int no;
 
 int main()
 {   ios_base::sync_with_stdio(false); cin.tie(0);   }
