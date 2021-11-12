22f045dd9f94f7cbcc425fec9ed2fba2e31114c3
Lucas Gabriel <lucasgabrielgarriel@hotmail.com>
2021-11-12 11:17:02 -0300

    Update

--- Codes/Paradigms/Knapsack_sem_Repetição.cpp
+++ Codes/Paradigms/Knapsack_sem_Repetição.cpp
@@ -1,8 +1,15 @@
-//URI-1767 usado como exemplo
+#include <bits/stdc++.h>
 
+using namespace std;
+
+#define MAX_N 10005
+typedef vector< int > vi;
+typedef vector<vector<int>> vvi;
+
+//Top-Down approach
 int Ans, N;
-int Price[350], Peso[100];
-int pd[150][100];
+int Price[MAX_N], Peso[MAX_N];
+int pd[MAX_N][100];
 vi VetRec;
 
 int Knap(int Ct, int W)
@@ -39,19 +46,64 @@ void Rec(int Ct, int W)
 		return Rec(Ct + 1, W);
 }
 
-int main()
-{	
-	memset(pd, -1, sizeof(pd));
-	int Ans = Knap(0, 50);
-	int PesoTot = 0;
-	Rec(0, 50);
+//----------------------------------------------------------------------------------//
+//Bottom-Up Approach
+void Reconstruc(vvi& dp, vector< vector< bool > >& Keep, 
+				vi& Pesos, vi& Valor, int N, int W)
+{
+	for (int i = N; i >= 0; --i)
+	{
+		if (Keep[i][W])
+		{
+			cout << "Item: " << i << ". Peso: " << Pesos[i - 1] 
+				<< ". Valor: " << Valor[i - 1] << '\n';
+			W -= Pesos[i - 1];
+		}
+	}
+}
 
-	printf("%d brinquedos\n", Ans);
-	for	(int i = 0; i < VetRec.size(); i++)
-		PesoTot += Peso[VetRec[i]];
-	printf("Peso: %d kg\n", PesoTot);
-	printf("sobra(m) %d pacote(s)\n\n", N - VetRec.size());
-	VetRec.clear();
+int Knapsack(int N, int W, vi& Pesos, vi& Valor)
+{
+	vvi dp(N + 1, vi(W + 1));
+	vector< vector< bool > > Keep(N + 1, vector< bool >(W + 1));
+	for (int i = 0; i <= W; ++i)
+		dp[0][i] = 0;
+	for (int i = 0; i <= N; ++i)
+		dp[i][0] = 0;
+	for (int k = 1; k <= N; ++k)
+	{
+		for (int d = 1; d <= W; ++d)
+		{
+			Keep[k][d] = Pesos[k - 1] <= d;
+			if (!Keep[k][d])
+				dp[k][d] = dp[k - 1][d];
+			else
+			{
+				int Op = dp[k - 1][d - Pesos[k - 1]] + Valor[k - 1];
+				if (Op > dp[k - 1][d])
+				{
+					Keep[k - 1][d] = false;
+					Keep[k][d] = true;
+					dp[k][d] = Op;
+				}
+				else
+				{
+					Keep[k][d] = false;
+					dp[k][d] = dp[k - 1][d];
+				}
+			}
+		}
+	}
 
-	return 0;
+	// std::cout << "Matriz da pd:\n";
+	// for (std::vector<int> i : dp)
+	// {
+	// 	for (int j : i)
+	// 		std::cout << j << ' ';
+	// 	std::cout << '\n';
+	// }
+
+	Reconstruc(dp, Keep, Pesos, Valor, N, W);
+
+	return dp[N][W];	//Maior valor possível
 }
