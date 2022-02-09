2689f8c2677e5174f38ab225a470dc91ce66c808
Lucas Gabriel <lucasgabrielgarriel@hotmail.com>
2022-02-09 14:34:48 -0300

    Update

--- Codes/Data_Struct/Segment_Tree.cpp
+++ Codes/Data_Struct/Segment_Tree.cpp
@@ -1,37 +1,42 @@
-#define MAX_N 100005
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAXN 100005
 #define Mid ((l + r) >> 1)
 typedef int no;
 
 /*
 * Find the sum on a interval and other stuff
 */
-int N, Vet[MAX_N], Seg[4 * MAX_N];
+int N, Vet[MAXN], Seg[4 * MAXN];
 
-no Modify(no a, no b)   //Function to do what I want
-{
+//Function to do what I want
+no Modify(no a, no b){
     return a + b;
 }
 
-no build(int id = 1, int l = 0, int r = N)
-{
+no build(int id = 1, int l = 0, int r = N){
     if (r - l < 2)  //[l, r)
         return Seg[id] = Vet[l];  //insert value on the interval
     Seg[id] = Modify(build(2 * id, l, Mid), build(2 * id + 1, Mid, r));
 }
 
-no Update(int i, int x, int l = 0, int r = N, int id = 1)
-{
-    if (r < i || i < l)
-        return Seg[id];
-    if (r - l < 2)
-        return Seg[id] = x; //Replace by x
-    return Modify(Update(i, x, l, Mid, 2 * id), Update(i, x, Mid, r, 2 * id + 1));
+void Update(int i, int x, int l = 0, int r = N, int id = 1){
+    if (r - l < 2){
+        Seg[id] = x; //Replace by x
+        return;
+    }
+    if (i < Mid)
+        Update(i, x, l, Mid, 2 * id);
+    else
+        Update(i, x, Mid, r, 2 * id + 1)
+    Seg[id] = Modify(Seg[2*id], Seg[2*id+1]);
 }
 
-int query(int i, int j, int l = 0, int r = N, int id = 1)
-{
-    if (r < i || j < l)
-        return 0;   //Don't change the answer
+int query(int i, int j, int l = 0, int r = N, int id = 1){ //[i, j)
+    if (i >= r || l >= j)
+        return inf;   //Don't change the answer
     if (i <= l && r <= j)
         return Seg[id];
     return Modify(query(i, j, l, Mid, 2 * id), query(i, j, Mid, r, 2 * id + 1));
