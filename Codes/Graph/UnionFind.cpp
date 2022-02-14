eaf1c279d519ed8274c5384a5f868b3320592c03
Lucas Gabriel <lucasgabrielgarriel@hotmail.com>
2022-02-14 14:04:38 -0300

    Update

---
+++ Codes/Graph/UnionFind.cpp
@@ -0,0 +1,35 @@
+//Union-Find, references CP4
+//Achar representande de conjuntos
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+
+class UnionFind{
+private:
+    vi p, rank, setSize;
+    int numSets;
+public:
+    UnionFind(int N){
+        p.assign(N, 0); for (int i = 0; i < N; ++i) p[i] = i;
+        rank.assign(N, 0);
+        setSize.assign(N, 1);
+        numSets = N;
+    }
+
+    int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
+    bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
+    int numDS(){ return numSets; }
+    int sizeOfSet(int i) { return setSize[findSet(i)]; }
+
+    void unionSet(int i, int j){
+        if (isSameSet(i, j)) return;
+        int x = findSet(i), y = findSet(j);
+        if (rank[x] > rank[y]) swap(x, y);
+        p[x] = y;
+        if (rank[x] == rank[y]) ++rank[y];
+        setSize[y] += setSize[x];
+        --numSets;
+    }
+};
--- template.cpp
+++ template.cpp
@@ -6,17 +6,18 @@ using namespace std;
 #define eulerconstant 0.577215664901532
 #define MAXN 100005
 #define LGN 17
+#define LSOne(S) ((int)(S) & -(int)(S))
 #define f first
 #define s second
-#define inf ~(1<<31)
-#define inf64 1000000000000000000
+#define inf 1e9
+#define inf64 4e18
 #define goldenratio ((double)(1 + sqrt(5)) / 2)
 #define isvalid(a_, b_, n_, m_) (a_ >= 0 && a_ < n_ && b_ >= 0 && b_ < m_)
 #define forn(i, n) for (int i = 0; i < (int)(n); ++i)
 #define rforn(i, n) for (int i = (int)(n); i >= 0; --i)
 #define forr(i, l, r) for (int i = (int)(l); i < (int)(r); ++i)
 #define rforr(i, l, r) for (int i = (int)(l); i >= (int)(r); --i)
-#define Mid ((l + r) >> 1)
+#define Mid (l + ((r - l) >> 1))
 #define filIn freopen("input.txt", "r", (__acrt_iob_func(0)));
 #define filOut freopen("output.txt", "w", (__acrt_iob_func(1)));
 
