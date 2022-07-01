4caa1ac1a761188acc444e8c38537031d92a9f8d
Lucas Gabriel <lucasgabrielgarriel@hotmail.com>
2022-07-01 18:18:54 -0300

    update

--- Codes/Data_Struct/Binary_Search_Tree.cpp
+++ Codes/Data_Struct/Binary_Search_Tree.cpp
@@ -3,36 +3,36 @@
 using namespace std;
 
 struct Node {
-	Node* left = NULL;
-	Node* right = NULL;
-	int key;
-	Node(int val){ key = val; }
+    Node* left = NULL;
+    Node* right = NULL;
+    int key;
+    Node(int val){ key = val; }
 };
 
 struct BinTree{
-	Node* root = NULL;
-	void insert(Node** R, int val){
-		if (!(*R))
-			*R = new Node(val);
-		else
-			if ((*R)->key > val)
-				insert(&(*R)->left, val);
-			else
-				insert(&(*R)->right, val);
-	}
-	bool busca(Node** R, int val){
+    Node* root = NULL;
+    void insert(Node** R, int val){
+        if (!(*R))
+            *R = new Node(val);
+        else
+            if ((*R)->key > val)
+                insert(&(*R)->left, val);
+            else
+                insert(&(*R)->right, val);
+    }
+    bool busca(Node** R, int val){
         if (!(*R)) return false;
-        if ((*R)->key == val){ return true; }
+        if ((*R)->key == val) return true;
         if ((*R)->key > val)
             return busca(&(*R)->left, val);
         return busca(&(*R)->right, val);
-	}
-	bool delAnt(Node **P, int val){
-	    if (!(*P))
+    }
+    bool delAnt(Node **P, int val){
+        if (!(*P))
             return false;
-	    if ((*P)->key > val)
+        if ((*P)->key > val)
             return delAnt(&(*P)->left, val);
-	    if ((*P)->key < val)
+        if ((*P)->key < val)
             return delAnt(&(*P)->right, val);
         if (!(*P)->left){
             (*P) = (*P)->right;
@@ -56,13 +56,13 @@ struct BinTree{
         (*P)->key = antecessor->key;
 
         return true;
-	}
-	bool delSuc(Node **P, int val){
-	    if (!(*P))
+    }
+    bool delSuc(Node **P, int val){
+        if (!(*P))
             return false;
-	    if ((*P)->key > val)
+        if ((*P)->key > val)
             return delSuc(&(*P)->left, val);
-	    if ((*P)->key < val)
+        if ((*P)->key < val)
             return delSuc(&(*P)->right, val);
         if (!(*P)->left){
             (*P) = (*P)->right;
@@ -86,27 +86,27 @@ struct BinTree{
         (*P)->key = sucessor->key;
 
         return true;
-	}
-	void preOrd(Node* P, vector<int> &vet){
-		if (P){
-			vet.push_back(P->key);
-			preOrd(P->left, vet);
-			preOrd(P->right, vet);
-		}
-	}
-	void inOrd(Node* P, vector<int> &vet){
-		if (P){
-			inOrd(P->left, vet);
-			vet.push_back(P->key);
-			inOrd(P->right, vet);
-		}
-	}
-	void posOrd(Node* P, vector<int> &vet){
-		if (P){
-			posOrd(P->left, vet);
-			posOrd(P->right, vet);
-			vet.push_back(P->key);
-		}
-	}
+    }
+    void preOrd(Node* P, vector<int> &vet){
+        if (P){
+            vet.push_back(P->key);
+            preOrd(P->left, vet);
+            preOrd(P->right, vet);
+        }
+    }
+    void inOrd(Node* P, vector<int> &vet){
+        if (P){
+            inOrd(P->left, vet);
+            vet.push_back(P->key);
+            inOrd(P->right, vet);
+        }
+    }
+    void posOrd(Node* P, vector<int> &vet){
+        if (P){
+            posOrd(P->left, vet);
+            posOrd(P->right, vet);
+            vet.push_back(P->key);
+        }
+    }
 };
 
