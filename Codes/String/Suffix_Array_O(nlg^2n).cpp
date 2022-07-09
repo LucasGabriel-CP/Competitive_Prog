a1686cf3ac73e8f369573175e892e0c4d004f9b1
Lucas Gabriel <lucasgabrielgarriel@hotmail.com>
2022-07-09 17:39:47 -0300

    update

--- Codes/String/Suffix_Array_O(nlg^2n).cpp
+++ Codes/String/Suffix_Array_O(nlg^2n).cpp
@@ -1,65 +1,55 @@
-int comp(vi R, int i, int j, int p) //p = 2^(k-1)
-{
-    if (i == j)
-        return 0;
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool comp(vector<int> R, int i, int j, int p){
     if (R[i] != R[j])
-        return R[i] - R[j];
-    return R[i + p] - R[j + p];
+        return R[i] < R[j];
+    return R[i + p] < R[j + p];
 }
 
-vector<int> build(string& Str)  //Construção do Array
-{
+vector<int> build(string& Str){
     int n = Str.length();
-    vi SA(n), R(n), Temp(n);
-    for (int i = 0; i < n; ++i)
-    {
+    vector<int> SA(n), R(n), Temp(n);
+    for (int i = 0; i < n; ++i){
         SA[i] = i;
         R[i] = Str[i];
     }
-    for (int p = 1; p < n; p *= 2)
-    {
-        sort(SA.begin(), SA.end(), [&](int i, int j) {  return comp(R, i, j, p) < 0 ? 1 : 0; });
+    for (int p = 0; p <= n; p = !p ? 1 :  p *= 2){
+        sort(SA.begin(), SA.end(), [&](int i, int j){ return comp(R, i, j, p);});
         Temp[SA[0]] = 0;
-        for (int i = 1, x = 0; i < n; ++i)
-        {
-            if (comp(R, SA[i], SA[i - 1], p) != 0)
-                ++x;
-            Temp[SA[i]] = x;
+        for (int i = 1; i < n; ++i){
+            Temp[SA[i]] = Temp[SA[i-1]];
+            if (comp(R, SA[i], SA[i - 1], p))
+                ++Temp[SA[i]];
         }
         R = Temp;
     }
 
-    SA.erase(SA.begin());
     return SA;
 }
 
-vi build_LCP(string& Str, vi SA) //Longest Common Prefix (precisa apagar a linha 67 pra funcionar perfeitamente)
-{
+vector<int> build_LCP(string& Str, vector<int> SA){
     int n = Str.length();
-    vi R(n), LCP(n);
+    vector<int> R(n), LCP(n);
     for (int i = 0; i < n; ++i) R[SA[i]] = i;
     int K = 0;
-    for (int i = 0; i < n; ++i)
-    {
-        if (i != n - 1)
-        {
+    for (int i = 0; i < n; ++i){
+        if (i != n - 1){
             int j = SA[R[i] + 1];
-            while (Str[i + K] == Str[j + K])
-                ++K;
+            while (K < n && Str[i + K] == Str[j + K]) ++K;
             LCP[R[i]] = K;
-            if (K)
-                --K;
+            if (K) --K;
         }
     }
 
     return LCP;
 }
 
-int main()
-{
-    string Str = "banana";
+int main(){
+    string Str = "LATAGATA";
     Str += '$';
-    vi SuffixArr = build(Str);
+    vector<int> SuffixArr = build(Str);
     for (int i : SuffixArr)
         printf("%d\n", i);
     return 0;
--- Codes/String/Suffix_Array_O(nlgn).cpp
+++ Codes/String/Suffix_Array_O(nlgn).cpp
@@ -1,10 +1,11 @@
-vector<int> build(string& Str)
-{
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> build(string const& Str){
     int n = Str.length();
     const int Alpha = 256;
-    vi SA(n), Rank(n), Cnt(max(Alpha, n), 0);
-
-    //Utilizando Counting Sort na primeira iteração
+    vector<int> SA(n), Rank(n), Cnt(max(Alpha, n), 0);
     for (int i = 0; i < n; ++i)
         Cnt[Str[i]]++;
     for (int i = 1; i < Alpha; ++i)
@@ -13,94 +14,86 @@ vector<int> build(string& Str)
         SA[--Cnt[Str[i]]] = i;
     Rank[SA[0]] = 0;
     int Classes = 1;
-    for (int i = 1; i < n; ++i)
-    {
+    for (int i = 1; i < n; ++i){
         if (Str[SA[i]] != Str[SA[i - 1]])
             Classes++;
         Rank[SA[i]] = Classes - 1;
     }
 
-    //Implementar o Radix Sort
-    //Pra evitar o uso da sort() da STL
-    vi TempSA(n), TempRank(n);
-    for (int h = 0; (1 << h) < n; ++h)
-    {
-        for (int i = 0; i < n; ++i)
-        {
+    vector<int> TempSA(n), TempRank(n);
+    for (int h = 0; (1 << h) < n; ++h){
+        for (int i = 0; i < n; ++i){
             TempSA[i] = SA[i] - (1 << h);
-            if (TempSA[i] < 0)
-                TempSA[i] += n;
+            if (TempSA[i] < 0) TempSA[i] += n;
         }
         fill(Cnt.begin(), Cnt.begin() + Classes, 0);
         for (int i = 0; i < n; ++i)
             Cnt[Rank[TempSA[i]]]++;
         for (int i = 1; i < Classes; ++i)
-            Cnt[i] += Cnt[i - 1];
-        for (int i = n - 1; i >= 0; --i)
+            Cnt[i] += Cnt[i-1];
+        for (int i = n-1; i >= 0; --i)
             SA[--Cnt[Rank[TempSA[i]]]] = TempSA[i];
         TempRank[SA[0]] = 0;
         Classes = 1;
-        for (int i = 1; i < n; ++i)
-        {
-            ii Cur = { Rank[SA[i]], Rank[(SA[i] + (1 << h)) % n] };
-            ii prev = { Rank[SA[i - 1]], Rank[(SA[i - 1] + (1 << h)) % n] };
+        for (int i = 1; i < n; ++i){
+            pair<int, int> Cur = { Rank[SA[i]], Rank[(SA[i] + (1 << h)) % n] };
+            pair<int, int> prev = { Rank[SA[i-1]], Rank[(SA[i-1] + (1 << h)) % n] };
             if (Cur != prev)
                 ++Classes;
-            TempRank[SA[i]] = Classes - 1;
+            TempRank[SA[i]] = Classes-1;
         }
         Rank.swap(TempRank);
     }
 
-    //SA.erase(SA.begin());
     return SA;
 }
 
-vi build_LCP(string& Str, vi SA)
-{
-    int n = Str.length();
-    vi R(n, 0), LCP(n - 1, 0);
-    for (int i = 0; i < n; ++i) R[SA[i]] = i;
-    int K = 0;
-    for (int i = 0; i < n; ++i)
-    {
-        if (R[i] != n - 1)
-        {
-            int j = SA[R[i] + 1];
-            while (Str[i + K] == Str[j + K])
-                ++K;
-            LCP[R[i]] = K;
-            if (K)
-                --K;
-        }
+vector<int> computeLCP(vector<int> const& SA, string const& T, int const& n){
+    vector<int> Phi(n), PLCP(n, 0), LCP(n);
+    Phi[SA[0]] = -1;
+    for (int i = 1; i < n; ++i)
+        Phi[SA[i]] = SA[i-1];
+    for (int i = 0, L = 0; i < n; ++i){
+        if  (Phi[i] == -1){ PLCP[i] = 0; continue; }
+        while ((i+L < n) && (Phi[i]+L < n) && (T[i+L] == T[Phi[i]+L])) ++L;
+        PLCP[i] = L;
+        L = max(L-1, 0);
     }
-
+    for (int i = 0; i < n; ++i)
+        LCP[i] = PLCP[SA[i]];
     return LCP;
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+int LCS(vector<int> const& SA, vector<int> const& LCP, int const& splitId, int const& n){
+    int maxLCP = -1;
+    for (int i = 1; i < n; ++i){
+        if ((SA[i] < splitId) == (SA[i-1] < splitId)) continue;
+        maxLCP = max(maxLCP, LCP[i]);
+    }
+    return maxLCP;
+}
 
-    string Str, Aux;
-    cin >> Str;
-    Str = Str + '$';
-
-    vi Alpha(27, 0);
-    vi SuffixArr = build(Str);
-    //printf("Suffix:\n");
-    //for (int i : SuffixArr)
-    //    printf("%d ", i);
-
-    int N = Str.size();
-    vi LCP = build_LCP(Str, SuffixArr);
-    for (int i = 1; i < N; i++) //Quantidade substrings, que inicia com cada letra, distintas
-        Alpha[Str[SuffixArr[i]] - 'a'] += N - 1 - SuffixArr[i] - LCP[i - 1];
-    //printf("\nALPHA:\n");
-    printf("%d", Alpha[0]);
-    for (int i = 1; i < 26; i++)
-        printf(" %d", Alpha[i]);
-    printf("\n");
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    string Str1, Str2, T, Aux;
+    //Uma string
+    cin >> Str1;
+    Str1 += '$';
+    T = Str1;
+    int n = (int)T.size();
+    vector<int> SA = build(T);
+    vector<int> LCP = computeLCP(SA, T, n);
+    //Mais de uma
+    cin >> Str1 >> Str2;
+    Str1 += '$';
+    Str2 += '#';
+    T = Str1;
+    T += Str2;
+    int splitId = (int)Str1.size();
+    n = (int)T.size();
+    vector<int> SuffixArr = build(T);
+    vector<int> LCP = computeLCP(SuffixArr, T, n);
+    cout << LCS(SuffixArr, LCP, splitId, n) << '\n';
 
     return 0;
 }
