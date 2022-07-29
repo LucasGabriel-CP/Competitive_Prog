08b7f83e5826e3248deeac34a04b852eb7555e9d
Lucas Gabriel <lucasgabrielgarriel@hotmail.com>
2022-07-29 11:43:13 -0300

    update

--- Codes/Paradigms/CoinChange.cpp
+++ Codes/Paradigms/CoinChange.cpp
@@ -1,11 +1,9 @@
-void CCDynamicP(vi Moedas, int Val) //O(Val*M)
-{
+//O(Val*M)
+void CCDynamicP(vector<int> const &Moedas, int Val){
 	int N = (int)Moedas.size();
-	vi table(Val + 1);
+	vector<int> table(Val + 1);
 	table[0] = 0;
-	
-	for (int c = 1; c <= Val; ++c)
-	{
+	for (int c = 1; c <= Val; ++c){
 		auto& dp = table[c] = inf;
 		for (int i = 0; i < N; ++i)
 			if (c >= Moedas[i])
@@ -15,33 +13,18 @@ void CCDynamicP(vi Moedas, int Val) //O(Val*M)
 	printf("%d\n", table[Val]);
 }
 
-void CCGreedy(vi Moedas, int Val) //O(Val), pode dar resultados incertos
-{
-	vi Ans;
+//O(Val)
+void CCGreedy(vector<int> const &Moedas, int Val){
+	vector<int> Ans;
 	sort(Moedas.begin(), Moedas.end());
-	
-	for (int i = 11; i >= 0; i--)
-	{
-		while (Val >= Moedas[i])
-		{
+	for (int i = 11; i >= 0; i--){
+		while (Val >= Moedas[i]){
 			Val -= Moedas[i];
 			Ans.push_back(abs(i - 12));
 		}
 	}
-
 	printf("%d\n", Ans.size());
 	for (int i = 0; i < Ans.size() - 1; i++)
 		printf("%d ", Ans[i]);
 	printf("%d\n", Ans[Ans.size() - 1]);
-}
-
-vi Moedas;
-bool CCValid(int S, int N) //O(min(N, S)), verifica se é possível atingir o valor S desejado
-{
-	if (S == 0)
-		return 1;
-	if (N == 0 || S < 0)
-		return 0;
-
-	return CCValid(S - Moedas[N - 1], N - 1) || CCValid(S, N - 1);
 }
--- Codes/Paradigms/Knapsack_com_Limite.cpp
+++ Codes/Paradigms/Knapsack_com_Limite.cpp
@@ -1,129 +1,62 @@
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <bitset>
-#include <queue>
-#include <set>
-#include <map>
-#include <cmath>
-#include <stack>
-#include <list>
-#include <limits.h>
-#include <list>
-#include <fstream>
-#include <string.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-#define pi 3.141592653589793
-#define sieveBOUND 100000
-#define inf32 1000000000
-#define inf64 1000000000000000000
-#define BOUNDS 1000000
-#define ll long long int
-#define ii pair<int, int>
-#define db double
-#define MOD 1000000007
-#define vi vector<int>
-#define limit 500010
-
-#define tos to_string
-
-#define sum(a, b) (ll)((((ll)(a % MOD) + (ll)(b % MOD)) + MOD) % MOD)
-#define mult(a, b) (((ll)(a % MOD) * (ll)(b % MOD)) % MOD)
-#define goldenratio ((double)(1 + sqrt(5)) / 2) 
-#define binet(a) (ll)round((((double)pow(goldenratio, a) - (double)pow(-goldenratio, -a)) / sqrt(5)))
-#define eulerconstant 0.577215664901532
-#define isvalid(a_, b_, n_, m_) (a_ >= 0 && a_ < n_ && b_ >= 0 && b_ < m_)
-
 int n;
-int value[60];
-int weight[60];
+int value[60], weight[60], dp[60[61];
 string nomes[60];
-int dp[60][51];
 
-int f(int at, int w)
-{
-	if (at >= n)
-		return 0;
-
-	if (dp[at][w] != -1)
-		return dp[at][w];
-
-	if (w < weight[at])
-		return dp[at][w] = f(at + 1, w);
-
+int f(int at, int w){
+	if (at >= n) return 0;
+	if (dp[at][w] != -1) return dp[at][w];
+	if (w < weight[at]) return dp[at][w] = f(at + 1, w);
 	int p1 = value[at] + f(at + 1, w - weight[at]);
 	int p2 = f(at + 1, w);
-
 	return dp[at][w] = max(p1, p2);
 }
 
 vector<string> resp;
-void rec(int at, int w)
-{
-	if (at >= n)
-		return;
-
-	if (w < weight[at])
-		return rec(at + 1, w);
-
+void rec(int at, int w){
+	if (at >= n)        return;
+	if (w < weight[at]) return rec(at + 1, w);
 	int p1 = value[at] + f(at + 1, w - weight[at]);
 	int p2 = f(at + 1, w);
-
-	if (f(at, w) == p1)
-	{
+	if (f(at, w) == p1){
 		resp.push_back(nomes[at]);
 		return rec(at + 1, w - weight[at]);
 	}
-	else
-		return rec(at + 1, w);
+	else return rec(at + 1, w);
 }
 
-int main()
-{
-	freopen("Input.txt", "r", stdin);
-
+int main(){
 	int c;
 	scanf("%d", &n);
 	scanf("%d", &c);
-
 	int aux = 0;
 	for (int i = 0; i < n; i++) {
 		int v, p, qt;
 		string nome;
 		scanf("%d %d %d", &v, &p, &qt);
 		cin >> nome;
-
 		int pot = 1;
-
 		while(qt > pot) {
 			value[aux] = v * pot;
 			weight[aux] = p * pot;
 			nomes[aux] = nome;
-
 			aux++;
 			qt -= pot;
 			pot *= 2;
 		}
-
 		value[aux] = v * qt;
 		weight[aux] = p * qt;
 		aux++;
 	}
-
 	n = aux;
-
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j <= c; j++)
 			dp[i][j] = -1;
-
 	printf("%d\n", f(0, 50));
 	rec(0, 50);
-
 	for (int i = 0; i < resp.size(); i++)
 		printf("%s\n", resp[i].c_str());
 	printf("\n");
