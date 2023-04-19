6e70c004878325da5f2eb876011bd6dcd65eea10
LucasGabriel-CP <lucasgabrielgarriel@hotmail.com>
2023-04-19 13:19:50 -0300

    update

--- template.cpp
+++ template.cpp
@@ -108,7 +108,7 @@ template<typename X, typename...Y> void dbg(X x, Y...y) { std::cerr << ' ' << x;
 #define all(var) (var).begin(),(var).end()
 
 #define rand() uid(rng)
-std::mt19937 rng(std::chrono::high_resolution_clock::now().time_since_epoch().count()); // ll = mt19937_64
+std::mt19937 rng(std::chrono::high_resolution_clock::now().time_since_epoch().count()); // i64 = mt19937_64
 std::uniform_int_distribution<int> uid(0, std::numeric_limits<int>::max());             // range
 
 /*-----------------------------------------------Templates-----------------------------------------------------------------------------------------------------*/
@@ -116,10 +116,6 @@ template<class T>inline int lb(const std::vector<T>&v,const T&x,int l=0,int r=-1
 template<class T>inline int ub(const std::vector<T>&v,const T&x,int l=0,int r=-1){return(int)(std::upper_bound(v.begin()+l,(r==-1?v.end():v.begin()+r),x)-v.begin());}
 template<class T>inline int lb(const T*v,int n,const T&x,int l=0,int r=-1){return(int)(std::lower_bound(v+l,v+(r==-1?n:r),x)-v);}
 template<class T>inline int ub(const T*v,int n,const T&x,int l=0,int r=-1){return(int)(std::upper_bound(v+l,v+(r==-1?n:r),x)-v);}
-template<class T,class T2>T min(T a,T2 b){return a<b?a:b;}
-template<class T,class T2>T max(T a,T2 b){return a>b?a:b;}
-template<class T,class ...T2>T min(T a,T2 ...b){return min(a,min(b...));}
-template<class T,class ...T2>T max(T a,T2 ...b){return max(a,max(b...));}
 template <typename T>
 using min_heap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
 using namespace __gnu_pbds;
@@ -128,11 +124,6 @@ using ordered_set = tree<T , null_type , std::less<T>, rb_tree_tag, tree_order_s
 using i64 = long long;
 using d16 = long double;
 
-/*-----------------------------------------------Constantes----------------------------------------------------------------------------------------------------*/
-const int inf = (int)1e9, LGN = 20, maxn = (int)2e5 + 5, mod = 1000000007;
-const i64 inf64 = (i64)4e18;
-const double eps = 1e-9;
-
 /*-----------------------------------------------Operadores----------------------------------------------------------------------------------------------------*/
 template<class X, class Y> std::istream& operator >> (std::istream& input, std::pair<X, Y>& _var) { input >> _var.first >> _var.second; return input; }
 template<class X> std::istream& operator >> (std::istream& input, std::vector<X>& _var) { for(X &x : _var) input >> x; return input; }
@@ -140,10 +131,11 @@ template<class X, class Y> std::ostream& operator << (std::ostream& output, std:
 template<class X> std::ostream& operator << (std::ostream& output, std::vector<X> const& _var) { for(int i = 0; i < sz(_var); i++) output << (i > 0 ? " " : "") << _var[i]; return output; }
 template<class X, class Y> std::ostream& operator << (std::ostream& output, std::map<X, Y> const& _var) { bool first = 1; for(auto const &[a, b] : _var) { if(first == 0) output << " "; first = 0; output << "{" << a << " : " << b << "}"; } return output; }
 template<class X> std::ostream& operator << (std::ostream& output, std::set<X> const& _var) { for(auto it = _var.begin(); it != _var.end(); it++) output << (it != _var.begin() ? " " : "") << *it; return output; }
-template<class...X> inline void read(X &...x) { ((std::cin >> x), ...); }
-template<class...X> inline void print(X const&...x) { ((std::cout << x), ...); }
 
-
+/*-----------------------------------------------Constantes----------------------------------------------------------------------------------------------------*/
+const int inf = (int)1e9, LGN = 20, maxn = (int)2e5 + 5, mod = 1000000007;
+const i64 inf64 = (i64)4e18;
+const double eps = 1e-9;
 
 int main(){
     std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr);
